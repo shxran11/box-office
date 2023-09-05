fe24ab3e85715bb186acea77753545be049d3f07
Sharanya <saranya.podder.fiem.cse21@teamfuture.in>
2023-09-05 11:02:30 +0530

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Starred from './pages/starred';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+        <Route path="*" element={<div>Not found</div>} />
+
+        {/*<Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="teams" element={<Teams />}>
+        <Route path=":teamId" element={<Team />} />
+        <Route path="new" element={<NewTeamForm />} />
+        <Route index element={<LeagueStandings />} />
+      </Route>
+    </Route>
+    <Route element={<PageLayout />}>
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/tos" element={<Tos />} />
+    </Route>
+  <Route path="contact-us" element={<Contact />} />*/}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
