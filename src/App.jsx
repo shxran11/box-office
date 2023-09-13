f4961d642e4cd2b2b4ad138d881d4f523028d055
Sharanya <saranya.podder.fiem.cse21@teamfuture.in>
2023-09-13 13:34:35 +0530

    Replaced useShowById with useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/home';
 import Starred from './pages/starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/show';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/starred" element={<Starred />} />
+          </Route>
 
-        <Route path="/shows/:showId" element={<Show />} />
+          <Route path="/shows/:showId" element={<Show />} />
 
-        <Route path="*" element={<div>Not found</div>} />
+          <Route path="*" element={<div>Not found</div>} />
 
-        {/*<Route path="/" element={<App />}>
+          {/*<Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="teams" element={<Teams />}>
         <Route path=":teamId" element={<Team />} />
@@ -30,8 +34,9 @@ function App() {
       <Route path="/tos" element={<Tos />} />
     </Route>
   <Route path="contact-us" element={<Contact />} />*/}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/show.jsx
+++ src/pages/show.jsx
@@ -1,25 +1,14 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import { getShowById } from '../Api/tvmaze';
 
 const Show = () => {
   const { showId } = useParams();
-
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(null);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
+  //const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
