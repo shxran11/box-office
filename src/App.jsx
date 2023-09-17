b8acff7f382c65dde01ab6e33356dee37dd3dd1c
Sharanya <saranya.podder.fiem.cse21@teamfuture.in>
2023-09-17 19:47:12 +0530

    Styled the navs using custom radio and styled components

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,27 @@ import Home from './pages/home';
 import Starred from './pages/starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/show';
+import { GlobalTheme } from './theme';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/starred" element={<Starred />} />
-          </Route>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
 
-          <Route path="/shows/:showId" element={<Show />} />
+            <Route path="/shows/:showId" element={<Show />} />
 
-          <Route path="*" element={<div>Not found</div>} />
-
-          {/*<Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="teams" element={<Teams />}>
-        <Route path=":teamId" element={<Team />} />
-        <Route path="new" element={<NewTeamForm />} />
-        <Route index element={<LeagueStandings />} />
-      </Route>
-    </Route>
-    <Route element={<PageLayout />}>
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/tos" element={<Tos />} />
-    </Route>
-  <Route path="contact-us" element={<Contact />} />*/}
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<div>Not found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
